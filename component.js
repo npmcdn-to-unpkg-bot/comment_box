567d5fc250a0eadfff0fc86d8108c85426240bbc
Kylie Le <kylie.tram.le@gmail.com>
2016-08-17 12:46:42 -0700

    dynamic arguments & props

--- component.js
+++ component.js
@@ -7,7 +7,7 @@ class Comment extends React.Component {
           {/* reading 'props' (aka arguments) from the CommentBox class */}
           {this.props.author}
           </p> 
-        <p className="coment-body">
+        <p className="comment-body">
           {this.props.body}
         </p>
         <div className="comment-footer">
@@ -24,24 +24,48 @@ class Comment extends React.Component {
 
 class CommentBox extends React.Component {
   render() {
+     const comments = this._getComments();
      return( 
       <div class="comment-box">
         <h3>Comments</h3>
-        <h4 class="comment-count">2 comments</h4>
+        <h4 class="comment-count">
+          {/* 0-multiple comments */}
+          {this._getCommentsTitle(comments.length)}
+        </h4>
           <div class="comment-list">
-            {/* refers to the 2 divs(comment & comment-footer) in Comment class */}
-            <Comment
-              author="Willy-Kun" body="Hey Hey Hey" />
-            <Comment
-              author="Wilbur" body="Cookies" />
+            {/* refers to comments in commentList; JSX can render arrays */}
+            {comments}
           </div>
       </div>
 
-
-
       );
-    
+    }
+
+  _getComments() { // return array of JSX elements
+    const commentList = [
+      { id: 1, author: 'Morgan Circuit', body: 'Great Picture!'},
+      { id: 2, author: 'Bending Bender', body: 'Excellent Stuff'}
+    ];
+
+   return commentList.map( (comment) => {    // return a new component for each
+    return (                                 // element in commentList
+      <Comment 
+        author={comment.author} body={comment.body} key={comment.id} /> // accessing comment properties
+    );             
+   });
+  
+  };
+
+  _getCommentsTitle(commentCount) {
+    if (commentCount === 0) {
+      return 'No comments yet';
+    } else if (commentCount === 1) {
+      return '1 comment'; 
+    } else {
+      return `${commentCount} comments`; // string template syntax
+    }
   }
+
 }
 
 ReactDOM.render(
